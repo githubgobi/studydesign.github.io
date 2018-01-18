fe41e3e54f94f8d3d92813a2f92e202c3cbfa301
Santhanam Veerasamy <Santhanam Veerasamy>
2018-01-18 10:14:55 +0530

    second commit

---
+++ js/google-sheets-html.js
@@ -0,0 +1,34 @@
+/*!
+ *
+ * Google Sheets To HTML v0.9a
+ *
+ * To use, simply replace the "tq?key=" value in the
+ * URL below with your own unique Google document ID
+ *
+ * The Google document's sharing must be set to public
+ *
+ */
+
+google.load('visualization', '1', {
+    packages: ['table']
+});
+var visualization;
+
+function drawVisualization() {
+    var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1JZ8vF6gcFKv9rQV_2c6HUhT2xa7PP5As2_wCnhRroOs/edit?usp=sharing');
+    query.setQuery('SELECT A, B, C, D, E, F label A "S.No", B "College Name", C "Address", D "URL", E "Campuse(s)", F "University"');
+    query.send(handleQueryResponse);
+}
+
+function handleQueryResponse(response) {
+    if (response.isError()) {
+        alert('There was a problem with your query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+        return;
+    }
+    var data = response.getDataTable();
+    visualization = new google.visualization.Table(document.getElementById('table'));
+    visualization.draw(data, {
+        legend: 'bottom'
+    });
+}
+google.setOnLoadCallback(drawVisualization);
